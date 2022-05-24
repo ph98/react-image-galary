8326cf25b2d5e5cf072211bec0fbc4d5a17fb5a9
Parham Heydari <heidari1377@gmail.com>
2022-05-24 18:09:21 +0430

    add .env and setTimeout (not tested)

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 ### TODO: 
-1. use env for API
-2. fix SVG
-3. Each image apart from the image URL has a title and a short description along with a date field which will be used for sorting.
-4. The application should check the API end-point (our mock generator) every 1 minute to check/fetch new images and add them at the bottom of the list.
+1. Each image apart from the image URL has a title and a short description along with a date field which will be used for sorting.
--- src/components/images-page/images-page.tsx
+++ src/components/images-page/images-page.tsx
@@ -1,24 +1,39 @@
 import axios from 'axios'
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import toast from 'react-hot-toast'
 import { ImageProps } from '../image-item/image-item'
 import { ImageViewer } from '../image-viewer'
 import { ImagesList } from '../images-list'
 import './style.scss'
 
+const SERVER_ADDRESS =
+  process.env.REACT_APP_SERVER_ADDRESS ||
+  'https://apimocha.com/bearbulltraders/api'
+
 function ImagesPage() {
   const [images, setImages] = useState<Array<ImageProps>>([])
   const [activeImageId, setActiveImageId] = useState<number | null>(null)
   const [loading, setLoading] = useState(true)
 
-  useEffect(() => {
+  const fetchImages = useCallback(() => {
+    // the mock doesn't have date field for sorting, we can add a sort to it here:
+    // and we also can filter images with the same Id
     axios
-      .get('https://apimocha.com/bearbulltraders/api')
-      .then(({ data }) => setImages(data))
+      .get(SERVER_ADDRESS)
+      .then(({ data }) => setImages((imagesState) => [...imagesState, data]))
       .catch((err) => {
         toast.error(err.message)
       })
-      .finally(() => setLoading(false))
+      .finally(() => {
+        setTimeout(() => {
+          fetchImages()
+        }, 2000)
+        setLoading(false)
+      })
+  }, [])
+
+  useEffect(() => {
+    fetchImages()
   }, [])
 
   return (
